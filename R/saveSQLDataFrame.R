87ca261eb0c08450a2797730af79823bf3d998ad
qian <qliu7@buffalo.edu>
2019-03-20 16:35:40 -0400

    saveSQLDataFrame works for 'union'-ed SQLDataFrame.

--- R/saveSQLDataFrame.R
+++ R/saveSQLDataFrame.R
@@ -18,76 +18,68 @@
 
 saveSQLDataFrame <- function(x, dbname = tempfile(fileext = ".db"), 
                              dbtable = deparse(substitute(x)),
-                             ## overwrite = FALSE, ## couldn't pass to
-                             ## "copy_to" then "compute"
-                             types = NULL, ...)
+                             overwrite = FALSE, ...)
 {
     browser()
-    if (!file.exists(dbname))
-        file.create(dbname) 
-    dbname <- file_path_as_absolute(dbname)
+    if (file.exists(dbname)) {
+        dbname <- file_path_as_absolute(dbname)
+        if (overwrite == FALSE)
+            stop("The 'dbname' already exists! Please provide a new value ",
+                 "OR change 'overwrite = TRUE'. ")
+    }
 
-    ## open the existing connection
+    ## use the existing connection (if "op_union")
     con <- .con_SQLDataFrame(x)
-    ops <- x@tblData$ops
+    ## Write database table.
+    sql_cmd <- dbplyr::db_sql_render(con, x@tblData)
+    dbExecute(con, build_sql("CREATE TABLE ", dbtable, " AS ", sql_cmd))
+    ## error if "dbtable" already exist. "Error: table aa already exists"
+    file.copy(dbname(x), dbname, overwrite = overwrite)
+    msg_saveSQLDataFrame(x, dbname, dbtable)
+}
 
-    ## save intermediate y's as list elements
-    res <- list()
-    res[[1]] <- ops$y
-    chk <- ops$x$ops$x
-    if(is(chk, "op_double")) {
-        i <- 2
-        repeat{
-            res[[i]] <- chk$y
-            xx <- chk$x
-            chk <- chk$x$ops$x
-            i <- i+1
-            if (is(chk, "op_single")) {
-                res[[i]] <- xx
-                break        
-            }
-        }
-    }
+## ## open a new connection to the new "dbname". 
+##     copy_to(con, res[[length(res)]], name = dbtable,
+##             temporary = FALSE, overwrite = TRUE,  ## overwrite? 
+##             unique_indexes = NULL, indexes = list(dbkey(x)),
+##             analyze = TRUE, ...)
 
-    ## open a new connection to the new "dbname". 
-    copy_to(con, res[[length(res)]], name = dbtable,
-            temporary = FALSE, overwrite = TRUE,  ## overwrite? 
-            unique_indexes = NULL, indexes = list(dbkey(x)),
-            analyze = TRUE, ...)
-
-    for (i in rev(seq_len(length(res)-1))){
-        sql_tbl_append <- dbplyr::db_sql_render(con, res[[i]])
-        ## check ROWNAMES, only append unique rows
-        dbExecute(con, build_sql("INSERT INTO ", sql(dbtable), " ", sql_tbl_append))
+##     for (i in rev(seq_len(length(res)-1))){
+##         sql_tbl_append <- dbplyr::db_sql_render(con, res[[i]])
+##         ## check ROWNAMES, only append unique rows
+##         dbExecute(con, build_sql("INSERT INTO ", sql(dbtable), " ", sql_tbl_append))
         
         
-    }
-    ## dbGetQuery(con, paste0("SELECT * FROM ", in_schema()))
-    ## open a new connection to write database table.
-    con <- DBI::dbConnect(RSQLite::SQLite(), dbname = dbname)
-    ## attach the dbname of the to-be-copied "lazy tbl" to the new connection.
-    ## FIXME: possible to attach an online database?
-    auxName <- "aux"
-    DBI::dbExecute(con, paste0("ATTACH '", dbname(x), "' AS ", auxName))
+##     }
+##     ## dbGetQuery(con, paste0("SELECT * FROM ", in_schema()))
+##     ## open a new connection to write database table.
+##     con <- DBI::dbConnect(RSQLite::SQLite(), dbname = dbname)
+##     ## attach the dbname of the to-be-copied "lazy tbl" to the new connection.
+##     ## FIXME: possible to attach an online database?
+##     auxName <- "aux"
+##     DBI::dbExecute(con, paste0("ATTACH '", dbname(x), "' AS ", auxName))
     
-    ## open the to-be-copied "lazy tbl" from new connection.
-    tbl1 <- tbl(con, in_schema("aux", ident(dbtable(sdf1))))
-    ## apply all @indexes to "tbl_dbi" object (that opened from destination connection).
-    tbl1 <- .extract_tbl_from_SQLDataFrame_indexes(tbl1, sdf1) ## reorder by "key + otherCols"
+##     ## open the to-be-copied "lazy tbl" from new connection.
+##     tbl1 <- tbl(con, in_schema("aux", ident(dbtable(sdf1))))
+##     ## apply all @indexes to "tbl_dbi" object (that opened from destination connection).
+##     tbl1 <- .extract_tbl_from_SQLDataFrame_indexes(tbl1, sdf1) ## reorder by "key + otherCols"
 
-    copy_to(con, tbl1, name = dbtable,
-            temporary = FALSE, overwrite = TRUE,  ## overwrite? 
-            unique_indexes = NULL, indexes = list(dbkey(sdf1)),
-            analyze = TRUE, ...)
-    ## by default "temporary = FALSE", to physically write the table,
-    ## not only in the current connection. "indexes = dbkey(x)", to
-    ## accelerate the query lookup
-    ## (https://www.sqlite.org/queryplanner.html).
+##     copy_to(con, tbl1, name = dbtable,
+##             temporary = FALSE, overwrite = TRUE,  ## overwrite? 
+##             unique_indexes = NULL, indexes = list(dbkey(sdf1)),
+##             analyze = TRUE, ...)
+##     ## by default "temporary = FALSE", to physically write the table,
+##     ## not only in the current connection. "indexes = dbkey(x)", to
+##     ## accelerate the query lookup
+##     ## (https://www.sqlite.org/queryplanner.html).
     
+msg_saveSQLDataFrame <- function(x, dbname, dbtable) {
     msg <- paste0("## A new database table is saved! \n",
-                  "## Source: table<", dbtable, "> [", paste(dim(x),
-                  collapse = " X "), "] \n",
-                  "## Database: ", db_desc(con), "\n",
+                  "## Source: table<", dbtable, "> [",
+                  paste(dim(x), collapse = " X "), "] \n",
+                  ## "## Database: ", db_desc(con), "\n",    ## con??
+                  "## Database: ",
+                  paste("sqlite ", dbplyr:::sqlite_version(), " [", dbname, "] \n"),
                   "## Use the following command to reload into R: \n",
                   "## dat <- SQLDataFrame(\n",
                   "##   dbname = \"", dbname, "\",\n",
