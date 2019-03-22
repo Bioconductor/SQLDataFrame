8e26b1a3898521f2bd9b96d4914a94ae074d2578
qian <qliu7@buffalo.edu>
2019-03-22 17:21:18 -0400

    deactivated the dbtable() for SQLDataFrame with lazy tbls

--- R/SQLDataFrame-class.R
+++ R/SQLDataFrame-class.R
@@ -178,21 +178,22 @@ setMethod("dbtable", "SQLDataFrame", function(x)
     op <- x@tblData$ops
     if (is(op, "op_base_remote")) {
         return(as.character(op$x))
-    } else if (is(op, "op_join")) {
-        out1 <- op$x$ops$x
-        repeat {
-            out1 <- out1$x
-            if (is.ident(out1)) break
-        }
-        out2 <- op$y$ops$x
-        repeat {
-            out2 <- out2$x
-            if (is.ident(out2)) break
-        }
-        return(c(as.character(out1), as.character(out2)))
-        ## FIXME: print more informative msg here. 
-    } else if (is(op, "op_distinct")) {
-        
+    } ## else if (is(op, "op_join")) {
+    ##     out1 <- op$x$ops$x
+    ##     repeat {
+    ##         out1 <- out1$x
+    ##         if (is.ident(out1)) break
+    ##     }
+    ##     out2 <- op$y$ops$x
+    ##     repeat {
+    ##         out2 <- out2$x
+    ##         if (is.ident(out2)) break
+    ##     }
+    ##     return(c(as.character(out1), as.character(out2)))
+    ##     ## FIXME: print more informative msg here. 
+    ## } else if (is(op, "op_distinct")) {
+    else {
+        message("## not available for SQLDataFrame with lazy tbl.")
     }
 })
 
