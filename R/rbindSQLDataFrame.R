d2ca80f92e1b21ddea271d32a273febce76c73f9
qian <qliu7@buffalo.edu>
2019-03-27 14:31:27 -0400

    cleaning rbind duplicate codes

--- R/rbindSQLDataFrame.R
+++ R/rbindSQLDataFrame.R
@@ -1,76 +1,55 @@
 ## "union" method is defined for both S3 and S4 classes. Since
 ## SQLDataFrame is a S4 object, here I am using "setMethod" instead of
 ## "union.SQLDataFrame".
-
 ## "union" itself removes duplicates, and reorder by default using 1st column, then 2nd, ...
-## SQL::union_all, I guess, no automatic ordering...
+## SQL::union_all, I guess, no automatic ordering??
 
-## setMethod("union", signature = "SQLDataFrame", function(x, y, ...)
-## {
-##     browser()
-##     x1 <- .extract_tbl_from_SQLDataFrame(x)
-##     y1 <- .extract_tbl_from_SQLDataFrame(y)
-##     tbl.union <- dbplyr:::union.tbl_lazy(x1, y1)
-##     x@tblData <- tbl.union
-##     x@dbnrows <- tbl.union %>% summarize(n=n()) %>% pull(n)
-##     x@indexes <- vector("list", 2)
-##     return(x)
-## })
-
-## works as "rbind"... Be cautious to save the @tblData, which is
-## supposed to be unique rows. So need to modify the @tblData and
-## update @indexes. Call "union.tbl_lazy", reverse the automatic
-## ordering, and then update @indexes.
-
-## possible strategy:
-## 1. use "union", then reverse the ordering.
-## 2. use "union_all", then only extract unique lines. "SQL::unique?"
-
-## following function works like "union" without automatic
-## ordering. Could rename as just "union", and explain the strategy in
-## documentation. Extend to "rbind" function by updating @indexes
-## slot.
 #' @export
 
-## Since "dbplyr:::union.tbl_lazy(x,y)" only evaluates data from same source, need to rewrite, using the dbExecute(con, "ATTACH dbname AS aux")
 setMethod("union", signature = c("SQLDataFrame", "SQLDataFrame"), function(x, y, ...)
 {
     browser()
     tbls <- .join_union_prepare(x, y)
     tbl.out <- dbplyr:::union.tbl_lazy(tbls[[1]], tbls[[2]])
-  
-    x@tblData <- tbl.out
-    x@dbnrows <- tbl.out %>% summarize(n=n()) %>% pull(n)
-    x@indexes <- vector("list", 2)
-
+    dbnrows <- tbl.out %>% summarize(n=n()) %>% pull(n)
     ## recalculate the @dbconcatKey
-    x@dbconcatKey <- tbl.out %>%
-        mutate(concatKey = paste(!!!syms(dbkey(x)), sep="\b")) %>%
-        pull(concatKey)  
+    ## concatKeys <- tbl.out %>%
+    ##     mutate(concatKey = paste(!!!syms(dbkey(x)), sep="\b")) %>%
+    ##     pull(concatKey)  
     ## ?? using SQL index? 
     ## ## extract the new @dbconcatKey instead of recalculating
-    ## tt <- do.call(rbind, strsplit(c(ROWNAMES(x), ROWNAMES(y)), split = "\b"))
-    ## tt <- as.data.frame(tt, stringsAsFactors = FALSE)
-    ## cls <- x1 %>% head %>% select(dbkey(x)) %>% as.data.frame() %>% sapply(class)
-    ## for (i in seq_len(length(tt))) class(tt[,i]) <- unname(cls)[i]
-    ## tt[with(tt, order(!!!syms(dbkey(x)))), ] ## doesn't work ... 
-    
+    tt <- do.call(rbind, strsplit(c(ROWNAMES(x), ROWNAMES(y)), split = "\b"))
+    tt <- unique(as.data.frame(tt, stringsAsFactors = FALSE))
+    ## colnames(tt) <- dbkey(x)
+    cls <- tbl.out %>% head %>% select(dbkey(x)) %>% as.data.frame() %>% sapply(class)
+    for (i in seq_len(length(tt))) class(tt[,i]) <- unname(cls)[i]
+    od <- do.call(order, tt)
+    tt <- tt[od, ]
+    concatKeys <- do.call(paste, c(tt, sep="\b"))
+    x@tblData <- tbl.out
+    x@dbnrows <- dbnrows
+    x@dbconcatKey <- concatKeys
+    x@indexes <- vector("list", 2)
+    ## BiocGenerics:::replaceSlots(x, tblData = tbl.out,
+    ##                            dbnrows = dbnrows,
+    ##                            dbconcatKey = concatKeys,
+    ##                            indexes = vector("list", 2))
     return(x)
 })
 
 .join_union_prepare <- function(x, y)
 {
-    browser()  
+    ## browser()  
     if (is(x@tblData$ops, "op_double")) {
         con <- .con_SQLDataFrame(x)
-        x1 <- .extract_tbl_from_SQLDataFrame(x)  ## lazy tbl. 
-        dbs <- .dblist(con)
-
+        tblx <- .open_tbl_from_connection(con, "main", x)
+        
         if (is(y@tblData$ops, "op_double")) {
             ## attach all databases from y except "main", which is
             ## temporary connection from "union" or "join"
+            dbs <- .dblist(con)
             cony <- .con_SQLDataFrame(y)
-            y1 <- .extract_tbl_from_SQLDataFrame(y)
+            tbly <- .extract_tbl_from_SQLDataFrame(y)
             dbsy <- .dblist(cony)[-1,]
             
             idx <- match(paste(dbsy$name, dbsy$file, sep=":"), paste(dbs$name, dbs$file, sep=":"))
@@ -80,42 +59,33 @@ setMethod("union", signature = c("SQLDataFrame", "SQLDataFrame"), function(x, y,
                 .attach_database(con, dbsy[i, "file"], dbsy[i, "name"])
             }
             ## open the lazy tbl from new connection
-            sql_cmd <- dbplyr::db_sql_render(cony, y1)
-            y1 <- tbl(con, sql_cmd)
+            sql_cmd <- dbplyr::db_sql_render(cony, tbly)
+            tbly <- tbl(con, sql_cmd)
         } else {
-            aux_y <- dbs[match(dbname(y), dbs$file), "name"]
-            if (is.na(aux_y)) {
-                y1 <- .attach_and_open_tbl_in_new_connection(con, y)
-            } else {
-                y1 <- .open_tbl_from_connection(con, aux_y, y)
-            }
+            tbly <- .attachMaybe_and_open_tbl_in_new_connection(con, y)
         }
     } else if (is(y@tblData$ops, "op_double")) {  
         con <- .con_SQLDataFrame(y)
-        y1 <- .extract_tbl_from_SQLDataFrame(y)
-        dbs <- .dblist(con)
-        aux_x <- dbs[match(dbname(x), dbs$file), "name"]
-        if (is.na(aux_x)) {
-            x1 <- .attach_and_open_tbl_in_new_connection(con, x)
-        } else {
-            x1 <- .open_tbl_from_connection(con, aux_x, x)
-        }
+        tbly <- .open_tbl_from_connection(con, "main", y)
+        tblx <- .attachMaybe_and_open_tbl_in_new_connection(con, x)
     } else {
         dbname <- tempfile(fileext = ".db")
-        con <- DBI::dbConnect(RSQLite::SQLite(), dbname = dbname)        
-        ## attach database into the local connection.
-        x1 <- .attach_and_open_tbl_in_new_connection(con, x)
-        dbs <- .dblist(con)
-        aux_y <- dbs[match(dbname(y), dbs$file), "name"]
-        if (is.na(aux_y)) {
-            y1 <- .attach_and_open_tbl_in_new_connection(con, y)
-        } else {
-            y1 <- .open_tbl_from_connection(con, aux_y, y)
-        }
+        con <- DBI::dbConnect(RSQLite::SQLite(), dbname = dbname)
+        tblx <- .attachMaybe_and_open_tbl_in_new_connection(con, x)
+        tbly <- .attachMaybe_and_open_tbl_in_new_connection(con, y)
     }
-    return(list(x1, y1))
+    return(list(tblx, tbly))
 }
 
+.attachMaybe_and_open_tbl_in_new_connection <- function(con, sdf) {
+    ## browser()
+    dbs <- .dblist(con)
+    aux <- dbs[match(dbname(sdf), dbs$file), "name"]
+    if (is.na(aux))
+        aux <- .attach_database(con, dbname(sdf))
+    res_tbl <- .open_tbl_from_connection(con, aux, sdf)
+    return(res_tbl)
+}
 .dblist <- function(con) {
     res <- dbGetQuery(con, "PRAGMA database_list")
     return(res)
@@ -135,19 +105,17 @@ setMethod("union", signature = c("SQLDataFrame", "SQLDataFrame"), function(x, y,
         tblx <- .extract_tbl_from_SQLDataFrame(sdf)
     } else {
         auxSchema <- in_schema(aux, ident(dbtable(sdf)))
-        ## FIXME: dbtable() for SDF generated from union/join, how to extract the dbtable()? 
         tblx <- tbl(con, auxSchema)
         tblx <- .extract_tbl_from_SQLDataFrame_indexes(tblx, sdf)
     }
-    ## tblx$ops$args <- list(auxSchema)
     return(tblx)
 }
 
-.attach_and_open_tbl_in_new_connection <- function(con, sdf) {
-    aux <- .attach_database(con, dbname(sdf))
-    ## dbs <- dbGetQuery(con, "PRAGMA database_list")
-    .open_tbl_from_connection(con, aux, sdf)
-}
+## .attach_and_open_tbl_in_new_connection <- function(con, sdf) {
+##     aux <- .attach_database(con, dbname(sdf))
+##     ## dbs <- dbGetQuery(con, "PRAGMA database_list")
+##     .open_tbl_from_connection(con, aux, sdf)
+## }
 
 .rbind_SQLDataFrame <- function(..., deparse.level = 1)
 {
