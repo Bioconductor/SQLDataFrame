c1a002d5a6d2efd700fa1e9a384c7dcd33f8ad7d
qian <qliu7@buffalo.edu>
2019-09-25 14:51:59 -0400

    updated documentation and script clean up

--- R/saveSQLDataFrame.R
+++ R/saveSQLDataFrame.R
@@ -5,13 +5,20 @@
 #'     returns a \code{SQLDataFrame} object constructed from the
 #'     user-supplied \code{dbname}, \code{dbtable}, and \code{dbkey}.
 #' @param x The \code{SQLDataFrame} object to be saved.
+#' @param localConn A MySQL connection with write permission. Will be
+#'     used only when the input SQLDataFrame objects connects to MySQL
+#'     connections without write permission. A new MySQL table will be
+#'     written in the the database this argument provides.
 #' @param dbname A character string of the file path of to be saved
-#'     database file.
+#'     SQLite database file. Will only be used when the input
+#'     SQLDataFrame represents a SQLite database table. Default to
+#'     save in a temporary file.
 #' @param dbtable A character string for the to be saved database
 #'     table name. Default is the name of the input
 #'     \code{SQLDataFrame}.
 #' @param overwrite Whether to overwrite the \code{dbtable} if already
-#'     exists. Default is FALSE.
+#'     exists. Only applies to the saving of SQLDataFrame objects
+#'     representing SQLite database tables. Default is FALSE.
 #' @param index Whether to create the database index. Default is TRUE.
 #' @param ... other parameters passed to methods.
 #' @return A \code{SQLDataFrame} object.
@@ -30,13 +37,13 @@
 #' @export
 
 
-saveSQLDataFrame <- function(x, localConn = connSQLDataFrame(x),
-                             dbname = tempfile(fileext = ".db"),  ## only used for SQLiteConnection.
-                             dbtable = deparse(substitute(x)),
-                             overwrite = FALSE,
+saveSQLDataFrame <- function(x,
+                             dbname = tempfile(fileext = ".db"),  ## only used for SQLiteConnection
+                             dbtable = deparse(substitute(x)), 
+                             localConn = connSQLDataFrame(x),  ## only used for MySQLConnection
+                             overwrite = FALSE,  ## only used for SQLiteConnection
                              index = TRUE, ...)
 {
-    ## browser()
     if (is(connSQLDataFrame(x), "MySQLConnection")) {
         con <- connSQLDataFrame(x)
         if (identical(con, localConn)) {
@@ -47,31 +54,16 @@ saveSQLDataFrame <- function(x, localConn = connSQLDataFrame(x),
             sql_cmd <- build_sql("CREATE TABLE ", sql(dbtable), " AS ",
                                  db_sql_render(con, tbl), con = con) 
             dbExecute(con, sql_cmd)
-            ## error1 table exists: <simpleError in .local(conn,
-            ## statement, ...): could not run statement: Table 'sdf1'
-            ## already exists>
-            
-            ## error2 no write permission: <simpleError in .local(conn,
-            ## statement, ...): could not run statement: INSERT, CREATE
-            ## command denied to user
-            ## 'genome'@'c-67-99-175-226.roswellpark.org' for table
-            ## 'sdf1'>
         } else {
-            ## FIXME: check if lazy table (with queries) in remote
-            ## connection. If yes, create fedtable separately, and
-            ## pass old queries into new fed tables. If no, do the
-            ## following.
             if (is(tblData(x)$ops, "op_join"))
                 stop("Saving SQLDataFrame with lazy join queries ",
-                     "is not supported!")
+                     "from same non-writable MySQL database is not supported!")
             if (!.mysql_has_write_perm(localConn))
                 stop("Please provide a MySQL connection ",
                      "with write permission in argument: localConn")
-            fedtable <- dplyr:::random_table_name()  ## temporary,
-                                                     ## will be
-                                                     ## removed if
-                                                     ## saved
-                                                     ## correctly!
+            fedtable <- dplyr:::random_table_name()
+            ## a temporary federated table will be generated, and then
+            ## removed if "create table" is successful. 
             tbl <- .createFedTable_and_reopen_tbl(x,
                                                   localConn,
                                                   fedtable,
@@ -101,59 +93,41 @@ saveSQLDataFrame <- function(x, localConn = connSQLDataFrame(x),
                                                             ## here.
         } else if (is(tblData(x)$ops, "op_double") | is(tblData(x)$ops, "op_single")) { 
             con <- connSQLDataFrame(x)
-            tbl <- tblData(x)  ## Since the "*_join", "union" function
-                               ## returns @indexes as NULL. Only
-                               ## "rbind" will retain the
-                               ## @indexes[[1]], and which be
-                               ## processed as an additional file
-                               ## "dbtable_ridx". So here we only use
-                               ## "tblData(x)" instead of
-                               ## ".extract_tbl_from_SQLDataFrame_indexes"
+            tbl <- tblData(x)
+            ## Since the "*_join", "union" function returns @indexes
+            ## as NULL. Only "rbind" will retain the @indexes[[1]],
+            ## and which be processed as an additional file
+            ## "dbtable_ridx". So here we only use "tblData(x)"
+            ## instead of ".extract_tbl_from_SQLDataFrame_indexes"
+
             if (!is.null(ridx(x))) {  ## applies to SQLDataFrame from "rbind"
                 dbWriteTable(con, paste0(dbtable, "_ridx"),
                              value = data.frame(ridx = ridx(x)))
             }
         }
-        sql_cmd <- build_sql("CREATE TABLE ", sql(dbtable), " AS ", db_sql_render(con, tbl), con = con)
+        sql_cmd <- build_sql("CREATE TABLE ", sql(dbtable), " AS ",
+                             db_sql_render(con, tbl), con = con)
         dbExecute(con, sql_cmd)
     }
-    ## error if "dbtable" already exist. "Error: table aa already
-    ## exists". Not likely happen here, because SQLDataFrame generated
-    ## from "join" or "union" has connection to a new temporary .db
-    ## file with empty contents.
-    
     ## add unique index file with dbkey(x)
     if (index)
         dbplyr:::db_create_indexes.DBIConnection(con, dbtable,
                                                  indexes = list(dbkey(x)),
                                                  unique = TRUE)
     ## FIXME: implement "overwrite" argument here for the index
-    ## file. if (found & overwrite)
+    ## doesn't work.
     ## https://www.w3schools.com/sql/sql_create_index.asp DROP INDEX
     ## table_name.index_name; see also: dbRemoveTable()
 
-    ## following condition applies to both SQLite and MySQL,
-    ## operations don't. The @dbname from SQLite includes the path,
-    ## MySQL doesn't. For MySQL, dbGetInfo(connSQLDataFrame(x))$dbname
-    ## only include the database name.
     if (is(con, "SQLiteConnection")) {
         if(is(tblData(x)$ops, "op_double") | is(tblData(x)$ops, "op_single")) {
             file.copy(connSQLDataFrame(x)@dbname, dbname, overwrite = overwrite)
-        }
+        }  ## save database file into the provided "dbname" file path.
     }
     .msg_saveSQLDataFrame(x, con, dbtable)
-    ## currently SQLDataFrame() constructor required 'password', so disable for now.
     res <- SQLDataFrame(conn = con, dbtable = dbtable, dbkey = dbkey(x))
     invisible(res)
 }
-
-## .mysqlErrorMsg <- function(dbtable) {
-##     paste0("ERROR: \n", "1. Check you connection is still valid. \n",
-##            "2. Check if the table of '", dbtable,
-##            "' already exists! \n",
-##            "3. Make sure your provided MySQL connection ",
-##            "has write permission.")
-## }
 
 .msg_saveSQLDataFrame <- function(x, con, dbtable)
 {
