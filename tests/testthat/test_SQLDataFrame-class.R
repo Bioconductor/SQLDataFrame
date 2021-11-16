a91be3801a0e7e0a7a5d4823ed4cf34d6a668edf
Hervé Pagès <hpages.on.github@gmail.com>
2021-11-15 23:48:21 -0800

    Make SQLDataFrame a concrete subclass of virtual class DataFrame

--- tests/testthat/test_SQLDataFrame-class.R
+++ tests/testthat/test_SQLDataFrame-class.R
@@ -34,7 +34,7 @@ test_that("SQLDataFrame constructor works",
     ## check slot values / accessors
     expect_true(validObject(obj))
     exp <- c("dbkey", "dbnrows", "tblData", "indexes", "dbconcatKey")
-    expect_identical(exp, slotNames(obj))
+    expect_identical(exp, head(slotNames(obj), n=length(exp)))
     expect_identical(normalizePath(test.db), normalizePath(connSQLDataFrame(obj)@dbname))
     expect_identical("colData", dbtable(obj))
     expect_identical("sampleID", dbkey(obj))
